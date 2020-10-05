1c23d759867694ffbebe0c14bcff74ec2a95176f
Aleks Markovic <aleks.markovic182@gmail.com>
2020-10-05 22:00:22 +0200

    Added Unity project, camera from robot working

---
+++ unity/ROS Controller App/Assets/Scripts/Settings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField]
+    private InputField ipInputField;
+    [SerializeField]
+    private Button cameraButton;
+    
+    private void Awake()
+    {
+        cameraButton.onClick.AddListener(LoadCamera);
+    } 
+    
+    private void LoadCamera()
+    {        
+        SceneManager.LoadScene("Camera");
+    }
+
+    public void SaveHost()
+    {
+        PlayerPrefs.SetString("host", ipInputField.text);
+    }
+}
